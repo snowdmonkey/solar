a884ce93e898fb56f1c2d352cbbfd8df6e06dd95
Liu Xuefeng <xuefeng.liu@honeywell.com>
2017-08-28 18:20:07 +0800

    add defect visualization on leaflet map

--- backend/prototype/app/main.py
+++ backend/prototype/app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 defects_summary = None
-
+exif = None
 
 def get_defects_summary() -> dict:
     global defects_summary
@@ -14,8 +16,16 @@ def get_defects_summary() -> dict:
     return defects_summary
 
 
+def get_exif() -> dict:
+    global exif
+    if exif is None:
+        with open(r"C:\Users\h232559\Documents\projects\uav\pic\2017-06-21-funingyilin-DJI\6-21-FLIR\exif.json") as f:
+            exif = json.load(f)
+    return exif
+
+
 @app.route("/defects")
-def get_defects():
+def get_defects() -> str:
     defects = list()
     for defect_id, defect_info in get_defects_summary().items():
         defect = {"defectId": defect_id,
@@ -25,5 +35,22 @@ def get_defects():
     return json.dumps(defects)
 
 
+@app.route("/images/defect/<string:defect_id>")
+def get_images(defect_id: str) -> str:
+    """
+    return a json string which contains the names of the images relating to a defect
+    :param defect_id: string starts with "defect" and followed by an int, e.g., defect146
+    :return: json string
+    """
+    defect_info = get_defects_summary().get(defect_id)
+    image_names = defect_info.get("images")
+    results = list()
+    for image_name in image_names:
+        latitude = get_exif().get(image_name).get("GPSLatitude")
+        longitude = get_exif().get(image_name).get("GPSLongitude")
+        results.append({"imageName": image_name, "latitude": latitude, "longitude": longitude})
+    return json.dumps(results)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True, threaded=True)
