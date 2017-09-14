6c4efde61eb30fd263663ead0008fc1c1b3f4d31
Jason Jiao <jason.jiao@honeywell.com>
2017-09-14 11:14:04 +0800

    update docker-compose.yml with script

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,11 +1,43 @@
 from configparser import ConfigParser
+from os import sep
+from os.path import isdir, isfile
 import sys
 
-config = ConfigParser()
-config.read('conf/config.ini')
 
-section = sys.argv[1]
-if config.has_section(section):
-    print(config.items(section))
-else:
-    print('No configuration for %s' % section)
+class TenantInfo(object):
+    def __init__(self, section: str):
+        config = ConfigParser()
+        config.read('conf/config.ini')
+        if config.has_section(section):
+            self.host = config.get(section, 'HOST')
+            self.dbroot = config.get(section, 'DB_ROOT')
+            self.dataroot = config.get(section, 'DATA_ROOT')
+            self.panorama = config.get(section, 'PANORAMA')
+            self.gsdpanorama = config.get(section, 'GSD_PANORAMA')
+            self.gsd = config.get(section, 'GSD')
+        else:
+            print('No configuration for Tenant %s' % section)
+
+
+if __name__ == "__main__":
+    tinfo = TenantInfo(sys.argv[1])
+    if not isdir(sep.join((tinfo.dbroot, 'data'))) or not isdir(sep.join((tinfo.dbroot, 'conf'))):
+        print('Invalid DB_ROOT.')
+    elif not isdir(tinfo.dataroot):
+        print('Invalid DATA_ROOT')
+    elif not isfile(sep.join((tinfo.dataroot, tinfo.panorama))):
+        print('Invalid PANORAMA')
+    else:
+        try:
+            float(tinfo.gsdpanorama)
+        except ValueError:
+            print('Invalid GSD_PANORAMA')
+            exit(-1)
+        try:
+            float(tinfo.gsd)
+        except ValueError:
+            print('Invalid GSD')
+            exit(-1)
+        with open('docker-compose.template', 'r') as f:
+            for l in f:
+                print(l)
