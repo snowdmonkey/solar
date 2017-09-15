654f0e1e6116279442740f2fc862bbc5e78330b2
Jason Jiao <jason.jiao@honeywell.com>
2017-09-15 10:44:37 +0800

    generate index.html from index.html.template

---
+++ frontend/init.py
@@ -0,0 +1,30 @@
+import os
+from os import remove
+from os.path import isfile
+
+
+def get_conf():
+    ret = {}
+    for k in os.environ.keys():
+        if k.count('GPS') > 0:
+            ret.update({k: os.environ.get(k)})
+    if 'HOST' in os.environ.keys():
+        ret.update({'HOST': os.environ.get('HOST')})
+    return ret
+
+
+if __name__ == '__main__':
+    conf = get_conf()
+
+    path = 'html/index1.html'
+    if isfile(path):
+        remove(path)
+
+    with open('html/index.html.template', 'r', encoding='utf-8') as tfile:
+        with open(path, 'w', encoding='utf-8') as ofile:
+            for line in tfile:
+                if line.count('{$') > 0:
+                    for key in conf.keys():
+                        formatted = '{$%s}' % key
+                        line = line.replace(formatted, conf.get(key))
+                ofile.write(line)
