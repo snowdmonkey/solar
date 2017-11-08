43a13d9a379e4110fe9e3d2ed75dfd350ae6d591
Liu Xuefeng <xuefeng.liu@honeywell.com>
2017-11-08 17:47:17 +0800

    add station support to backend

--- backend/prototype/app/main.py
+++ backend/prototype/app/main.py
@@ -4,7 +4,6 @@ from os.path import join
 from image_process import ImageProcessPipeline
 from pymongo import MongoClient
 from typing import Union
-import json
 import cv2
 import io
 import logging
@@ -34,9 +33,9 @@ def get_image_root() -> str:
     return image_root_path
 
 
-def get_defects_summary(date: str) -> Union[None, dict]:
+def get_defects_summary(station: str, date: str) -> Union[None, dict]:
 
-    value = get_mongo_client().solar.defect.find_one({"date": date}, {"value": 1})
+    value = get_mongo_client().solar.defect.find_one({"date": date, "station": station}, {"value": 1})
     if value is None:
         defects_summary = None
     else:
@@ -44,9 +43,9 @@ def get_defects_summary(date: str) -> Union[None, dict]:
     return defects_summary
 
 
-def get_exif(date: str) -> Union[dict, None]:
+def get_exif(station: str, date: str) -> Union[dict, None]:
 
-    value = get_mongo_client().solar.exif.find_one({"date": date}, {"value": 1})
+    value = get_mongo_client().solar.exif.find_one({"station": station, "date": date}, {"value": 1})
     if value is None:
         exif = None
     else:
@@ -54,119 +53,163 @@ def get_exif(date: str) -> Union[dict, None]:
     return exif
 
 
-def get_rotated_folder(date: str) -> str:
+def get_rotated_folder(station: str, date: str) -> str:
     """
     :return: the folder where the rotated images are saved
     """
-    folder_path = join(get_image_root(), date, "ir/rotated")
+    folder_path = join(get_image_root(), station, date, "ir/rotated")
     return folder_path
 
 
-def get_visual_folder(date: str) -> str:
+def get_visual_folder(station: str, date: str) -> str:
     """
     :return: the folder where the raw visual images are saved
     """
-    folder_path = join(get_image_root(), date, "visual/rotated")
+    folder_path = join(get_image_root(), station, date, "visual/rotated")
     return folder_path
 
 
-@app.route("/defects", methods=["POST", "GET"])
-def get_defects() -> str:
-    if request.method == "GET":
+@app.route("/station/<str:station>/date", methos=["GET"])
+def get_reports_date_by_station(station: str):
+    """
+    return the dates of available reports for a station
+    """
+    posts = get_mongo_client().solar.defect.find({"station": station}, {"date": True})
+    result = [x.get("date") for x in posts]
+    return jsonify(result)
+
+
+@app.route("/station/<str:station>/date/<str:date>/defect", methods=["GET"])
+def get_defect_by_date_and_station(station: str, date: str):
+    """
+    return a defect list by station and date
+    """
+    defect_summary = get_defects_summary(station, date)
+    if defect_summary is None:
+        abort(404)
+    else:
         defects = list()
-        date = request.args.get("date")
-        defect_summary = get_defects_summary(date)
-        if defect_summary is None:
-            abort(404)
-        else:
-            for defect_id, defect_info in get_defects_summary(date).items():
-                defect = {"defectId": defect_id,
-                          "latitude": defect_info.get("latitude"),
-                          "longitude": defect_info.get("longitude"),
-                          "category": defect_info.get("category"),
-                          "groupId": defect_info.get("group")}
-                defects.append(defect)
-            return json.dumps(defects)
-    elif request.method == "POST":
-        date = request.form.get("date")
-        folder_path = join(get_image_root(), date)
-        pipeline = ImageProcessPipeline(image_folder=folder_path, date=date)
-        pipeline.run()
-        return "success"
+        for defect_id, defect_info in defect_summary.items():
+            defect = {
+                "defectId": defect_id,
+                "latitude": defect_info.get("latitude"),
+                "longitude": defect_info.get("longitude"),
+                "category": defect_info.get("category"),
+                "groupId": defect_info.get("group")
+            }
+            defects.append(defect)
+        return jsonify(defects)
 
 
-@app.route("/defect/<string:defect_id>/position", methods=["POST"])
-def set_defect_location(defect_id: str) -> str:
-    lat = float(request.form["lat"])
-    lng = float(request.form["lng"])
-    date = request.form["date"]
-    get_mongo_client().solar.defect.update_one(
-        {"date": date},
-        {"$set":
-             {"value.{}.latitude".format(defect_id): lat,
-              "value.{}.longitude".format(defect_id): lng}})
+@app.route("/station/<str:station>/date/<str:date>/defect", methods=["PUT"])
+def analyze_by_date_and_station(station: str, date: str):
+    folder_path = join(get_image_root(), station, date)
+    pipeline = ImageProcessPipeline(image_folder=folder_path, date=date)
+    pipeline.run()
     return "OK"
 
 
-@app.route("/defect/<string:defect_id>/category", methods=["POST"])
-def set_defect_category(defect_id: str) -> str:
-    date = request.form["date"]
-    category = float(request.form["category"])
-    get_mongo_client().solar.defect.update_one(
-        {"date": date},
-        {"$set": {"value.{}.category".format(defect_id): category}})
+@app.route("/station/<str:station>/date/<str:date>/defect/<str:defect_id>", methods=["PUT"])
+def set_defect_by_id(station: str, date: str, defect_id: str):
+    """
+    set a defect's gps coordinates and category
+    """
+    post_body = request.get_json()
+    lat = post_body.get("lat")
+    lng = post_body.get("lng")
+    cat = post_body.get("category")
+    defect_collection = get_mongo_client().solar.defect
+
+    if defect_collection.find_one({"station": station, "date": date}).get("value").get(defect_id) is None:
+        abort(404)
+
+    if lat is not None:
+        defect_collection.update_one({"station": station, "date": date},
+                                     {"$set": {"value.{}.latitude".format(defect_id): lat}})
+    if lng is not None:
+        defect_collection.update_one({"station": station, "date": date},
+                                     {"$set": {"value.{}.longitude".format(defect_id): lng}})
+    if cat is not None:
+        defect_collection.update_one({"station": station, "date": date},
+                                     {"$set": {"value.{}.category".format(defect_id): cat}})
     return "OK"
 
 
-@app.route("/images/defect")
-def get_images() -> str:
+@app.route("/station/<str:station>/date/<str:date>/defect/<str:defect_id>/image", methods=["GET"])
+def get_images_by_defect(station: str, date: str, defect_id: str):
     """
     return a json string which contains the names of the images relating to a defect
     :return: json string
     """
-    date = request.args.get("date")
-    defect_id = request.args.get("defectId")
-    defect_info = get_defects_summary(date).get(defect_id)
+    defect_info = get_defects_summary(station, date).get(defect_id)
     image_names = defect_info.get("images")
     results = list()
-    exif = get_exif(date)
+    exif = get_exif(station, date)
     if exif is None:
         abort(404)
     for image_name in image_names:
         latitude = exif.get(image_name).get("GPSLatitude")
         longitude = exif.get(image_name).get("GPSLongitude")
         results.append({"imageName": image_name, "latitude": latitude, "longitude": longitude})
-    return json.dumps(results)
+    return jsonify(results)
 
 
-@app.route("/image/labeled")
-def get_labeled_image():
+@app.route("/station/<str:station>/date/<str:date>/image/ir/<str:image>", methods=["GET"])
+def get_labeled_image(station: str, date: str, image: str):
     """
     generate and return an image with image name and defect id, the corresponding defects should be labeled on the image
+    label a defect if a defect_id is provided
     :return:
     """
-    base_image_name = request.args.get("image")
+    base_image_name = image
     image_name = base_image_name + ".jpg"
     defect_id = request.args.get("defect")
-    date = request.args.get("date")
-    rects = get_defects_summary(date).get(defect_id).get("images").get(base_image_name)
-    img = cv2.imread(join(get_rotated_folder(date), image_name), cv2.IMREAD_COLOR)
-    for rect in rects:
-        x, y, w, h = rect.get("x"), rect.get("y"), rect.get("w"), rect.get("h")
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    color_map = request.args.get("colorMap")
+
+    img = cv2.imread(join(get_rotated_folder(station, date), image_name), cv2.IMREAD_COLOR)
+
+    mapping = {
+        "autumn": cv2.COLORMAP_AUTUMN,
+        "bone": cv2.COLORMAP_BONE,
+        "jet": cv2.COLORMAP_JET,
+        "winter": cv2.COLORMAP_WINTER,
+        "rainbow": cv2.COLORMAP_RAINBOW,
+        "ocean": cv2.COLORMAP_OCEAN,
+        "summer": cv2.COLORMAP_SUMMER,
+        "spring": cv2.COLORMAP_SPRING,
+        "cool": cv2.COLORMAP_COOL,
+        "hsv": cv2.COLORMAP_HSV,
+        "pink": cv2.COLORMAP_PINK,
+        "hot": cv2.COLORMAP_HOT}
+
+    rect_color = (0, 0, 255)
+
+    if color_map is not None:
+        rect_color = (255, 255, 255)
+        color_id = mapping.get(color_map)
+        if color_id is None:
+            abort(400, "illegal color map")
+        else:
+            img = cv2.applyColorMap(img, color_id)
+
+    if defect_id is not None:
+        rects = get_defects_summary(station=station, date=date).get(defect_id).get("images").get(base_image_name)
+        for rect in rects:
+            x, y, w, h = rect.get("x"), rect.get("y"), rect.get("w"), rect.get("h")
+            cv2.rectangle(img, (x, y), (x + w, y + h), rect_color, 1)
+
     img_bytes = cv2.imencode(".png", img)[1]
     return send_file(io.BytesIO(img_bytes), attachment_filename="labeled.png", mimetype="image/png")
 
 
-@app.route("/image/visual")
-def get_visual_image():
+@app.route("/station/<str:station>/date/<str:date>/image/visual/<str:image>", methods=["GET"])
+def get_visual_image(station: str, date: str, image: str):
     """
     :return: raw visual image specified by image name
     """
-    base_image_name = request.args.get("image")
+    base_image_name = image
     image_name = base_image_name + ".jpg"
-    date = request.args.get("date")
-    img = cv2.imread(join(get_visual_folder(date), image_name), cv2.IMREAD_COLOR)
+    img = cv2.imread(join(get_visual_folder(station=station, date=date), image_name), cv2.IMREAD_COLOR)
     img_bytes = cv2.imencode(".png", img)[1]
     return send_file(io.BytesIO(img_bytes), attachment_filename="visual.png", mimetype="image/png")
 
