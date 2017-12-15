1829ee92929e401fbf4b7fc86a63fabf82fd71dc
Jason Jiao <jason.jiao@honeywell.com>
2017-12-15 15:01:21 +0800

    update app setup process

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-REGISTRY="159.99.255.163:5000"
-
-
 cd $BASEDIR
 
 docker run -it --rm -v $PWD:/usr/src/app/conf -v /opt:/opt setup python setup.py $1
 
-docker-compose up -d
+if [ -f docker-compose.yml ] then;
+	docker-compose -d
+
 
